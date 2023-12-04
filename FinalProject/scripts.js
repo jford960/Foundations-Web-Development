b92e1b6f030556b4b4d88a8af828c5d547017aa7
jford960 <jford960@gmail.comm>
2023-12-03 21:57:10 -0600

    Dropdown icons, backgrounds, some images

--- FinalProject/scripts.js
+++ FinalProject/scripts.js
@@ -1,19 +1,26 @@
-let headerBtns = document.getElementsByClassName('btnHeader');
-let btnContents = document.getElementsByClassName('btnContent');
-let footerBtns = document.getElementsByClassName('btnFooter')
+let headerBtns = document.getElementsByClassName("btnHeader");
+let btnContents = document.getElementsByClassName("btnContent");
+let footerBtns = document.getElementsByClassName("btnFooter");
 
 for (let index = 0; index < headerBtns.length; index++) {
-    headerBtns[index].addEventListener('click', function(){useButton(index)});
-    footerBtns[index].addEventListener('click', function(){useButton(index)});
+	headerBtns[index].addEventListener("click", function () {
+		useButton(index);
+	});
+	footerBtns[index].addEventListener("click", function () {
+		useButton(index);
+	});
 }
 
 function useButton(n) {
-    if (btnContents[n].style.display == 'block') {
-        btnContents[n].style.display = 'none'
-        btnContents[n].toggleAttribute('active')
-    }
-    else {
-        btnContents[n].style.display = 'block'
-        btnContents[n].toggleAttribute('active')
-    }
+	if (btnContents[n].style.display == "grid") {
+		btnContents[n].style.display = "none";
+		footerBtns[n].style.display = "none";
+		headerBtns[n].getElementsByClassName("material-symbols-outlined")[0].innerHTML =
+			"expand_more";
+	} else {
+		btnContents[n].style.display = "grid";
+		footerBtns[n].style.display = "grid";
+		headerBtns[n].getElementsByClassName("material-symbols-outlined")[0].innerHTML =
+			"expand_less";
+	}
 }
